1c431df2183e2623b08da27b8bdb12edb8028694
brotherlogic <brotherlogic@gmail.com>
2016-12-26 13:02:18 -0800

    Implemented ability to list devices. This closes #2.

--- server.go
+++ server.go
@@ -1,12 +1,47 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
+
+	"golang.org/x/net/context"
 
 	pb "github.com/brotherlogic/wink2rpc/proto"
 )
 
+// Retriever - Bridge for doing http requests
+type Retriever interface {
+	retrieve(url string) []byte
+}
+
+type jsonUnmarshaller interface {
+	Unmarshal([]byte, interface{}) error
+}
+type prodUnmarshaller struct{}
+
+func (jsonUnmarshaller prodUnmarshaller) Unmarshal(inp []byte, v interface{}) error {
+	return json.Unmarshal(inp, v)
+}
+
+type device struct {
+	Name string
+}
+
+type listDevicesResponse struct {
+	Data []device
+}
+
 // ListDevices calls out to list all the wink devices
 func (s *Server) ListDevices(ctx context.Context, in *pb.Empty) (*pb.DeviceList, error) {
-	return &pb.DeviceList{}, nil
+	url := "https://api.wink.com/users/me/wink_devices"
+	page := s.retr.retrieve(url)
+
+	response := listDevicesResponse{}
+	s.marshaller.Unmarshal(page, &response)
+
+	list := &pb.DeviceList{}
+	for _, device := range response.Data {
+		list.Device = append(list.Device, &pb.Device{Name: device.Name})
+	}
+
+	return list, nil
 }
--- server_test.go
+++ server_test.go
@@ -1,17 +1,48 @@
 package main
 
 import (
-	"context"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
 	"testing"
+
+	"golang.org/x/net/context"
 
 	pb "github.com/brotherlogic/wink2rpc/proto"
 )
 
-func TestListDevices(t *testing.T) {
+type TestRetriever struct{}
+
+func (retriever TestRetriever) retrieve(URL string) []byte {
+	strippedURL := strings.Replace(strings.Replace(URL[21:], "?", "_", -1), "&", "_", -1)
+	blah, err := os.Open("testdata/" + strippedURL)
+	if err != nil {
+		log.Printf("Error opening test file %v", err)
+	}
+	body, _ := ioutil.ReadAll(blah)
+	return body
+}
+
+// Gets a test server that'll pull from local files rather than reading out
+func getTestServer() Server {
 	s := Server{}
-	_, err := s.ListDevices(context.Background(), &pb.Empty{})
+	s.retr = TestRetriever{}
+	s.key = "madeupkey"
+	s.marshaller = prodUnmarshaller{}
+	return s
+}
+
+func TestListDevices(t *testing.T) {
+	s := getTestServer()
+	list, err := s.ListDevices(context.Background(), &pb.Empty{})
 
 	if err != nil {
 		t.Errorf("Failure to list devices: %v", err)
+	}
+
+	//Test results should have one device called winner
+	if len(list.Device) != 1 || list.Device[0].Name != "winner" {
+		t.Errorf("Error in listing devices: %v", list)
 	}
 }
