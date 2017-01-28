35ee1e76e071e1e6b70254ba5e31b8c15349d7e0
Simon Tucker <brotherlogic@gmail.com>
2017-01-28 14:46:55 -0800

    Fixed popen command

--- BuildAndRun.py
+++ BuildAndRun.py
@@ -9,7 +9,7 @@ if os.path.isfile('hash'):
     current_hash = open('hash').readlines()[0]
 new_hash = os.popen('git rev-parse HEAD').readlines()[0]
 open('hash','w').write(new_hash)
-    
+
 # Move the old version over
 for line in os.popen('cp ' + name + ' old' + name).readlines():
     print line.strip()
@@ -26,8 +26,8 @@ size_1 = os.path.getsize('./old' + name)
 size_2 = os.path.getsize('./' + name)
 
 running = len(os.popen('ps -ef | grep ' + name).readlines()) > 3
-              
+
 if size_1 != size_2 or new_hash != current_hash or not running:
     for line in os.popen('killall ' + name).readlines():
         pass
-    subprocess.Popen(['./' + name, '--token=' + sys.argv[1]]])
+    subprocess.Popen(['./' + name, '--token=' + sys.argv[1]])
